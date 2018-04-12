af5fe84d4d33777cfccca9859463e8a3a94fe053
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-04-12 18:01:29 -0500

    wip

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -3,8 +3,12 @@ const petOutputDiv = document.getElementById('pets');
 const petDom = (pets) => {
   let petString = '';
   pets.forEach((pet) => {
-    petString += `<div>`;
+    petString += `<div class="pet-container col-sm-4">`;
     petString += `<h1>${pet.name}</h1>`;
+    petString += `<img src=${pet.imageUrl}>`;
+    petString += `<h2>${pet.color}</h2>`;
+    petString += `<p>${pet.specialSkill}</p>`;
+    petString += `<h2>${pet.type}</h2>`;
     petString += `</div>`;
   });
   return petString;
