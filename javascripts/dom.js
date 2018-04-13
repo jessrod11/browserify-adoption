9d3988eb7fcfdb7e8b97fb1dcb4214e3c3a0b1a2
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-04-12 20:01:11 -0500

    commiting again

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -3,9 +3,9 @@ const petOutputDiv = document.getElementById('pets');
 const petDom = (pets) => {
   let petString = '';
   pets.forEach((pet) => {
-    petString += `<div class="pet-container col-sm-4">`;
+    petString += `<div class="pet-container">`;
     petString += `<h1>${pet.name}</h1>`;
-    petString += `<img src=${pet.imageUrl}>`;
+    petString += `<img class="pet-image" src=${pet.imageUrl}>`;
     petString += `<h2>${pet.color}</h2>`;
     petString += `<p>${pet.specialSkill}</p>`;
     petString += `<h2>${pet.type}</h2>`;
