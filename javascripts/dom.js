f9d7ba356b1169ebd2a3415d041aad59ffc73892
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-04-27 22:58:16 -0500

    lost my code had to re-write

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -5,7 +5,7 @@ const petOutputDiv = document.getElementById('pets');
 const petDom = pets => {
   let petString = '';
   pets.forEach(pet => {
-    petString += `<div class="col-sm-4 text-center pet-container">`;
+    petString += `<div class=" ${pet.type} col-sm-4 text-center pet-container">`;
     petString += `<div class="panel panel-default">`;
     petString += `<div class="panel-body">`;
     petString += `<h1>${pet.name}</h1>`;
--- javascripts/events.js
+++ javascripts/events.js
@@ -1,21 +1,83 @@
-const showPets = e => {
-  const allCards = document.getElementsByClassName('pet-container');
-  for (let i = 0; i < allCards.length; i++) {
-    console.log('e', e);
-    const domPetType = e.target.parentNode.parentNode.childNodes[7].children[0].children[0].children[1].children[0].dataset.typeName;
-    if (domPetType === 'cat') {
-      allCards.classList.remove('hide');
-    } else if (domPetType === 'dog') {
-    } else {
-      allCards.classList.add('hide');
-    }
-  }
+const cats = document.getElementsByClassName('cat');
+const dogs = document.getElementsByClassName('dog');
+const dinos = document.getElementsByClassName('dino');
+
+const showCats = () => {
+  for (let i = 0; i < dogs.length; i++) {
+    const allDogs = dogs[i];
+    allDogs.classList.add('hide');
+  };
+  for (let j = 0; j < dinos.length; j++) {
+    const allDinos = dinos[j];
+    allDinos.classList.add('hide');
+  };
+  for (let x = 0; x < cats.length; x++) {
+    const allCats = cats[x];
+    allCats.classList.remove('hide');
+  };
 };
 
-const addButtonEvents = () => {
+const showDogs = () => {
+  for (let i = 0; i < dogs.length; i++) {
+    const allDogs = dogs[i];
+    allDogs.classList.remove('hide');
+  };
+  for (let j = 0; j < dinos.length; j++) {
+    const allDinos = dinos[j];
+    allDinos.classList.add('hide');
+  };
+  for (let x = 0; x < cats.length; x++) {
+    const allCats = cats[x];
+    allCats.classList.add('hide');
+  };
+};
+
+const showDinos = () => {
+  for (let i = 0; i < dogs.length; i++) {
+    const allDogs = dogs[i];
+    allDogs.classList.add('hide');
+  };
+  for (let j = 0; j < dinos.length; j++) {
+    const allDinos = dinos[j];
+    allDinos.classList.remove('hide');
+  };
+  for (let x = 0; x < cats.length; x++) {
+    const allCats = cats[x];
+    allCats.classList.add('hide');
+  };
+};
+
+const showAll = () => {
+  for (let i = 0; i < dogs.length; i++) {
+    const allDogs = dogs[i];
+    allDogs.classList.remove('hide');
+  };
+  for (let j = 0; j < dinos.length; j++) {
+    const allDinos = dinos[j];
+    allDinos.classList.remove('hide');
+  };
+  for (let x = 0; x < cats.length; x++) {
+    const allCats = cats[x];
+    allCats.classList.remove('hide');
+  };
+};
+
+const addButtonEvents = (e) => {
   const petButtons = document.getElementsByClassName('pet-button');
   for (let i = 0; i < petButtons.length; i++) {
-    petButtons[i].addEventListener('click', showPets);
+    const allPetBtns = petButtons[i];
+    allPetBtns.addEventListener('click', (e) => {
+      const buttonTarget = e.target.innerHTML;
+      if (buttonTarget === 'Cat') {
+        showCats();
+      } else if (buttonTarget === 'Dog') {
+        showDogs();
+      } else if (buttonTarget === 'Dino') {
+        showDinos();
+      } else {
+        showAll();
+      }
+    });
   }
 };
 
